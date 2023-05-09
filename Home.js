60c76db3933900dfd56bd255efefa41936380076
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-05-09 12:00:29 -0700

    added BC tourism project

---
+++ Home.js
@@ -0,0 +1,7 @@
+/* const searchBtn = document.querySelector(".searchIcon"),
+  searchBox = document.querySelector(".search-container");
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.toggle("active");
+}); */
+
